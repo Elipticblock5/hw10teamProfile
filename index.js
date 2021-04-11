9cd95ccf70525d55b950ebbdfde5cfc51f64a504
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-04-11 12:22:13 -0700

    adding prompts

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const theEmployees = [];
+
+function initApp () {
+    runHtml();
+    appendMember();
+}
+
+function appendMember() {
+    inquirer.prompt([{
+        message: "Enter member name:",
+        name: "name"
+    },
+
+    {
+    type: "list",
+    message: "choose a role",
+    choices: [
+        "Manager",
+        "Engineer",
+        "Intern",
+    ],
+    name: "role"
+},
+
+{
+    message: "Enter member's ID:",
+    name: "id"
+},
+{
+    message: "Enter members email:",
+    name: "email",
+}])
+
