96776d5f86c2f5087dbdc5b9a74cd4563900bae0
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-04-11 13:41:28 -0700

    adding new questions index

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer);
-const Intern = require("./lib/Intern");
+const questions = [
+    {
+        type: "input",
+        name: "firstName",
+        message: "what is employee's FIRST name?",
+        validate: nameQuestions => {
+            if (nameQuestions) {
+                return true;
+            }
+
+            else {
+                console.log('Enter the FIRST name please!');
+                return false;
+            }
+        }
+    },
+]
+
+
 
