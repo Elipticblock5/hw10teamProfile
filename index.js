5f73b09c88b63fcca6021df705968b62ee5e6e39
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-04-11 12:53:30 -0700

    added array information

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
+const Engineer = require("./lib/Engineer); 
 const Intern = require("./lib/Intern");
 
 const theEmployees = [];
@@ -33,25 +33,31 @@ function appendMember() {
     message: "Enter member's ID:",
     name: "id"
 },
+
 {
     message: "Enter members email:",
-    name: "email",
+    name: "email"
 }])
 
 .then(function({name, role, id, email}) {
     let roleAnswer = "";
     if (role === "Engineer") {
         roleAnswer = "GitHub username"
-    } else if (role === "Intern") {
+    } 
+    
+    else if (role === "Intern") {
         roleAnswer = "school name";
-    } else {
+    } 
+    
+    else {
         roleAnswer = "office phone";
     }
-}
-inquirer.prompt([{
+
+    inquirer.prompt([{
     message: `Enter member's $(roleAnswer)`,
     name: "roleAnswer"
 },
+
 {
     type: "list",
     message: "would you like to add another?",
@@ -61,4 +67,19 @@ inquirer.prompt([{
     ],
     name: "extraMembers"
 }])
+
+    .then(function({roleAnswer,extraMembers}) {
+        let addedMember;
+        if (role === "Engineer") {
+            addedMember = new Engineer(name, id, email, roleAnswer);
+        }
+
+        else if (role === "Intern") {
+            addedMember = new Intern(name, id, email, roleAnswer);
+        }
+
+        else {
+            addedMember = new Manager(name, id, email, roleAnswer)
+        }
+    })
 )
